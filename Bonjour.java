323a70d2c2e6f2aae167380d2d1228be20471564
Rodolphe33 <48446836+Rodolphe33@users.noreply.github.com>
2020-05-06 11:55:27 +0200

    Compilateur OK

--- Bonjour.java
+++ Bonjour.java
@@ -1,6 +1,12 @@
-class Bonjour {
-    public static void main(final String[] args) {
-        final String greeting = "Bonjour !";
-        System.out.println(greeting);
+// public class Bonjour {
+//     public static void main(final String[] args) {
+//         final String greeting = "Hello World !";
+//         System.out.println(greeting);
+//     }
+// }
+
+public class Bonjour {
+    public static void main (String[] args){
+        System.out.println("Hello W");
     }
 }
